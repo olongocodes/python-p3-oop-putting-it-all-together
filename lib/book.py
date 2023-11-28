f8a1310f6b6e07c21d6d12763b1b82e530b1872e
Kevin <kevinolongo9@gmail.com>
2023-11-29 01:08:47 +0300

    Complete assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None  # Use a private attribute to store page_count initially
+        self.page_count = page_count  # Use the property setter to set the initial value
+        self.current_page = 1
+    def get_book(self):
+        return self._page_count  # Return the private attribute
+
+    def set_book(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        self.current_page += 1
+
+    page_count = property(get_book, set_book)
+
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  # Use a private attribute to store size initially
+        self.size = size  # Use the property setter to set the initial value
+        self.repaired = False  # Initialize repaired status to False
+        self.condition = "Used"  # Initialize condition to "Used"
+
+    def get_size(self):
+        return self._size  # Return the private attribute
+
+    def set_size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.repaired = True
+        self.condition = "New"
+
+    size = property(get_size, set_size)
+
+                    
